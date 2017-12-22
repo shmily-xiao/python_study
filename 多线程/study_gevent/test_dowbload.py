1424c1c502236ec3f3d963f4426be2ff2bb8f732
wangzaijun <wangzaijun1234@126.com>
2017-12-22 16:17:27 +0800

    map加速访问

--- 多线程/study_gevent/test_dowbload.py
+++ 多线程/study_gevent/test_dowbload.py
@@ -3,11 +3,22 @@ monkey.patch_all()
 
 import urllib2
 from gevent.pool import Pool
+import requests
 
 def download(url):
-    return urllib2.urlopen(url).read()
+    # return urllib2.urlopen(url).read()
+    kwargs={}
+    if kwargs.get('verify') == None:
+        kwargs['verify'] = False
+    if kwargs.get('timeout') == None:
+        kwargs['timeout'] = 10
+    if kwargs.get('allow_redirects') == None:
+        kwargs['allow_redirects'] = False
+    r = requests.request("GET", url, **kwargs)
+    data = r.json()
+    return data
 
 if __name__ == '__main__':
-    urls = ['http://httpbin.org/get'] * 200
-    pool = Pool(20)
+    urls = ['https://10.143.160.250/lcx/scan/api/task/result/45245508-5a33-4199-b2f0-047388be7457'] * 20
+    pool = Pool(2)
     print pool.map(download, urls)
