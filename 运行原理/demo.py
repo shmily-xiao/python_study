80b7a8405449fdb64028ca68a1c1e7363396fefe
shmily <wangzaijun1234@126.com>
2018-08-22 00:10:49 +0800

    python 运行原理

--- 运行原理/demo.py
+++ 运行原理/demo.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import foo
+import time
 
 a = [1, 'python']
 
@@ -14,6 +15,38 @@ def func():
 
 print a
 
+
+def func1(a=1+1, b=[], c={}, t = time.time()):
+    print a
+    a += 1
+    print a
+
+    print id(b)
+    b.append("func1")
+    print b
+
+    print c
+    c["func1"]="func1"
+    print c
+
+    print t
+    print " ---------- "
+
+def fun2(d = 8*22, b=[], f={}):
+    print d
+    d += 1
+    print d
+
+    print id(b)
+    b.append("func2")
+    print b
+
+    print f
+    f["func2"] = "func2"
+    print f
+    print " ---------- "
+
+
 if __name__ == '__main__':
     func()
     foo.add(1,2)
@@ -22,3 +55,10 @@ if __name__ == '__main__':
     print f.b
     print f.funnc
     print func
+
+
+    func1()
+    func1()
+    print "------"
+    fun2()
+    fun2()
